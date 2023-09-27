fab126de2722025ecaf8af2f395a950bc3c2c2a9
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-09-27 12:51:05 +0300

    add consumer

--- producer_app/src/main/java/com/kafka/producer_app/ProducerAppApplication.java
+++ producer_app/src/main/java/com/kafka/producer_app/ProducerAppApplication.java
@@ -19,15 +19,27 @@ public class ProducerAppApplication {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
-        ProducerRecord<String, String> producerRecord =
-                new ProducerRecord<>("test_topic", "Hello from producer");
-        producer.send(producerRecord, (metadata, exception) -> {
-            if (exception == null) {
-                System.out.println("Message sent successfully. Offset: " + metadata.offset());
-            } else {
-                System.err.println("Error occurred while sending message: " + exception.getMessage());
+        for (int i = 0; i < 10; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        });        producer.flush();
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<>("test_topic", "Mess from producer number " + i);
+            producer.send(producerRecord, (metadata, exception) -> {
+                if (exception == null) {
+                    System.out.println("Received new metadata. \n" +
+                            "Topic:" + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition() + "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    System.err.println("Error while producing" + exception);
+                }
+            });
+            producer.flush();
+        }
         producer.close();
     }
 }
