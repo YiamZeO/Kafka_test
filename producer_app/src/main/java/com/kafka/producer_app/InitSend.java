6e7062ec202d035fd2aa089ccb4f2263553a3921
ZeO <102325825+YiamZeO@users.noreply.github.com>
2023-10-03 13:22:12 +0300

    Update InitSend.java

--- producer_app/src/main/java/com/kafka/producer_app/InitSend.java
+++ producer_app/src/main/java/com/kafka/producer_app/InitSend.java
@@ -3,6 +3,7 @@ package com.kafka.producer_app;
 import com.kafka_app.classes.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
